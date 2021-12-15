e070c75a1aeb2c07cf0fa516b075f2dca1c03959
JuanWigg <jgwiggenhauser@gmail.com>
2021-12-15 15:16:46 -0300

    Adds access to AWS Account and adds subnets

--- acme.tf
+++ acme.tf
@@ -1,8 +1,7 @@
 #Defino el proveedor de Cloud
 provider "aws" {
     region = "us-east-1"
-    access_key = "mi_access_key"
-    secret_key = "mi_secret_key"
+    shared_credentials_file = "/Users/Piche/.aws/credentials"
 }
 
 
