d7178154e32ad01c0a5fb37c6d76bf0b67a0d5bb
JuanWigg <jgwiggenhauser@gmail.com>
2021-12-15 14:51:06 -0300

    Adds VPC and Subnet configuration

--- acme.tf
+++ acme.tf
@@ -4,3 +4,42 @@ provider "aws" {
     access_key = "mi_access_key"
     secret_key = "mi_secret_key"
 }
+
+
+#Defino primero la VPC 
+resource "aws_vpc" "vpc-acme"{
+    cidr_block = "172.16.0.0/16"
+    tags = {
+        Name = "ACME_Prod_VPC"
+    }
+}
+
+
+
+#Defino las subnets para la VPC
+resource "aws_subnet" "ACMEPublica" {
+    vpc_id = aws_vpc.vpc-acme.id
+    cidr_block = "172.16.0.0/28"
+
+    tags = {
+        Name = "ACMEPublica"
+    }
+}
+
+resource "aws_subnet" "ACMEEmpleados" {
+    vpc_id = aws_vpc.vpc-acme.id
+    cidr_block = "172.16.4.0/22"
+
+    tags = {
+        Name = "ACMEEmpleados"
+    }
+}
+
+resource "aws_subnet" "ACMEServicios" {
+    vpc_id = aws_vpc.vpc-acme.id
+    cidr_block = "172.16.1.0/24"
+
+    tags = {
+        Name = "ACMEServicios"
+    }
+}
